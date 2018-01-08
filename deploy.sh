0fd53f7f25caf50ffa524f0655794183cb2daa8b
Gabriel <gabriel@garox.org>
2018-01-08 12:07:54 -0300

    travis deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  npm run compile
+  npm run build
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
