b5793e01817f784f238ac87fe1292ad69520b0bd
Gabriel <gabriel@garox.org>
2018-01-19 19:00:48 -0300

    css icon for vendor prefix

--- src/pages/Navigate.js
+++ src/pages/Navigate.js
@@ -2,21 +2,11 @@ import React from 'react'
 
 import { goto, Animated } from '../utilities'
 import { icon } from '../images'
-
-const iconStyle = { 
-    height: '105px',
-    width: 'auto',
-    display: 'block',
-    margin: '0 auto',
-    animationName: 'spin',
-    animationDuration: '5s',
-    animationIterationCount: 'infinite',
-    animationTimingFunction: 'ease'
-}
+import '../utilities/icon.css'
 
 const Navigate = ({ handleChange, weblink }) => (
     <section id='sec2'>
-        <img src={icon} style={iconStyle} alt='logo'/>
+        <img src={icon} className='icon' alt='logo'/>
         <Animated>
             <h1>STEEMSITES</h1>
             <h2>Decentralize the web.</h2>
