63ce89cd737376965b5bf965534f0204ba7f71c3
Garox <gabriel@garox.org>
2018-02-07 12:35:37 -0300

    remove log

--- src/utilities/goto.js
+++ src/utilities/goto.js
@@ -9,7 +9,6 @@ function goto (e, weblink) {
 async function awaitMagnetLink (author, permlink) {
     try {
         const { result, replies } = await getInParallel(author, permlink)
-        console.info(result)
         let { magnetLink } = JSON.parse(result.json_metadata) // eslint-disable-next-line
         if (magnetLink == undefined) throw 'No steemsites found'
         notify.show(`Starting to download '${result.title}'`, 'success')
