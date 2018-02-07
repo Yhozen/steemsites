6427f6b208412112e9dcf8e550848569a13b2dcc
Garox <gabriel@garox.org>
2018-02-07 13:18:23 -0300

    change icon

--- src/utilities/getList.js
+++ src/utilities/getList.js
@@ -3,10 +3,10 @@ import { peerweb } from './peerweb'
 import i18next from './international'
 
 export default function getList (sites) {
-    if (!sites) return (<li>{i18next.t('discoverEmpty')}</li>)
+    if (!sites) return (<li key={Math.random()}>{i18next.t('discoverEmpty')}</li>)
     let listOfSites = []
     for (let name of Object.keys(sites)) {
-        listOfSites.push(<li onClick={() => peerweb.render(sites[name], name)}>{name.toLocaleUpperCase()}</li>)
+        listOfSites.push(<li key={Math.random()} onClick={() => peerweb.render(sites[name], name)}>{name.toLocaleUpperCase()}</li>)
     }
     return listOfSites
 }
